659a4eea707e229b7649e9339ac58599fbf492c0
Obinna Anaenugwu <96635979+Matrixk1ng@users.noreply.github.com>
2023-09-12 21:56:04 +0000

    comit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
+
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+const double PI = acos(-1);
 
 int main()
 {
-    cout << "bruh" << endl;
-    cout << "bruh momento";
+    double radius, area;
+
+    cout << "Please enter the radius of the circle in inches: ";
+    cin >> radius;
+
+    cout << "PI = " << PI << endl; 
+    
+    area = radius * radius * PI;
+
+    cout << "\nA circle with a radius of " << radius
+         << " inches has an area of "
+         << area << " square inches.\n";
+    
+
     return 0;
 }
+
 
